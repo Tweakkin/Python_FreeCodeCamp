2f9e8b2c57058f4d2722ce308daf0f45dddf7f18
yd7w <yahyaboukhmira7@gmail.com>
2025-05-20 19:10:31 +0200

    Learn string manipulation by building a cipher

---
+++ vigenere_cipher.py
@@ -0,0 +1,18 @@
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+
+        if not char.isalpha():
+            final_message += char
+        else:        
+            key_char = key[key_index % len(key)]
+            key_index += 1
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
